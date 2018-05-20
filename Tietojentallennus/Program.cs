a8ddddc99cf9a0f21c85c10cfd08c2fc52ebd274
Joona Janhunen <joona.janhunen@stud.pointcollege.fi>
2018-05-20 17:26:10 +0300

    Lisätty VanhaLämpötila.txt

--- Tietojentallennus/Program.cs
+++ Tietojentallennus/Program.cs
@@ -1,15 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Tietojentallennus
+namespace TiedonTallennus
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string tiedostoLämpötila = @"C:\Tiedontallennusharjoitus\Lämpötila.txt";
+            string tiedostoVanhaLämpötila = @"C:\Tiedontallennusharjoitus\VanhaLämpötila.txt";
+
+            // luetaan tiedostosta aiemmin asetettu lämpötila
+            if (File.Exists(tiedostoLämpötila))
+            {
+                string aiempiArvo = File.ReadAllText(tiedostoLämpötila);
+                Console.WriteLine("Aiemmin asetettu lämpötila on " +
+                    aiempiArvo + " astetta.");
+                File.WriteAllText(tiedostoVanhaLämpötila, aiempiArvo.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Tervetuloa!");
+            }
+
+            int lämpötila = 0;
+            Console.Write("Anna uusi lämpötila: ");
+            string syöte = Console.ReadLine();
+            lämpötila = int.Parse(syöte);
+
+            // tiedostoon kirjoittaminen
+            File.WriteAllText(tiedostoLämpötila, lämpötila.ToString());
+            Console.WriteLine("Uusi lämpötila tallennettu.");
+
+            Console.ReadLine();
         }
     }
 }
